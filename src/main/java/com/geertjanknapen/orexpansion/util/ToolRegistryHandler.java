3636e0423ba77f9d0220b58f7f81c4ceba7cd2f7
Geert-Jan Knapen <geertjanknapen@gmail.com>
2020-10-08 14:56:48 +0200

    1-PICKAXE: Add two pickaxes and their textures.

--- src/main/java/com/geertjanknapen/orexpansion/util/ToolRegistryHandler.java
+++ src/main/java/com/geertjanknapen/orexpansion/util/ToolRegistryHandler.java
@@ -3,6 +3,7 @@ package com.geertjanknapen.orexpansion.util;
 import com.geertjanknapen.orexpansion.OreXpansion;
 import com.geertjanknapen.orexpansion.tools.OrexpansionItemTier;
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -17,12 +18,21 @@ public class ToolRegistryHandler {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    // Tools
+    // Tools - Swords
     public static final RegistryObject<SwordItem> SILVER_SWORD = ItemRegistryHandler.ITEMS.register("silver_sword", () ->
             new SwordItem(OrexpansionItemTier.SILVER, 3, -1.4F, new Item.Properties().group(OreXpansion.TAB))
     );
 
     public static final RegistryObject<SwordItem> STEEL_SWORD = ItemRegistryHandler.ITEMS.register("steel_sword", () ->
             new SwordItem(OrexpansionItemTier.STEEL, 4, -1.0F, new Item.Properties().group(OreXpansion.TAB))
+    );
+
+    // Tools - Pickaxes
+    public static final RegistryObject<PickaxeItem> SILVER_PICKAXE = ItemRegistryHandler.ITEMS.register("silver_pickaxe", () ->
+            new PickaxeItem(OrexpansionItemTier.SILVER, 0, 1.0F, new Item.Properties().group(OreXpansion.TAB))
+    );
+
+    public static final RegistryObject<PickaxeItem> STEEL_PICKAXE = ItemRegistryHandler.ITEMS.register("steel_pickaxe", () ->
+            new PickaxeItem(OrexpansionItemTier.STEEL, 0, 1.0F, new Item.Properties().group(OreXpansion.TAB))
     );
 }
